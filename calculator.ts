cb6afbba8c9ee4248bbaf9beff4213e86d2f1800
Martín <martingaliottimartinez@gmail.com>
2024-02-01 17:08:28 -0300

    Probando creación de tipos

--- calculator.ts
+++ calculator.ts
@@ -1,5 +1,19 @@
-const operations = ["multiply", "add", "divide", "subtract"];
+type Operations = "multiply" | "add" | "divide" | "subtract";
 
-const calculator = (a: number, b: number, op: string) => {};
+const calculator = (a: number, b: number, op: Operations) => {
+  switch (op) {
+    case "multiply":
+      return a * b;
+    case "add":
+      return a + b;
+    case "divide":
+      if (b === 0) return "can't divide by 0!";
+      return a / b;
+    case "subtract":
+      return a - b;
+    default:
+      throw new Error("Operation is not multiply, add, divide, or subtract!");
+  }
+};
 
-calculator(1, 2, "cualquiera");
+console.log(calculator(1, 2, "add"));
