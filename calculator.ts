2333a11df025ad0424821ff3ff84908a7d4a26ef
Martín <martingaliottimartinez@gmail.com>
2024-02-02 18:39:27 -0300

    Obteniendo valores de process.argv

--- calculator.ts
+++ calculator.ts
@@ -1,19 +1,41 @@
 type Operations = "multiply" | "add" | "divide" | "subtract";
 
-const calculator = (a: number, b: number, op: Operations) => {
+const calculator = (a: number, b: number, op: Operations): number => {
   switch (op) {
     case "multiply":
       return a * b;
     case "add":
       return a + b;
     case "divide":
-      if (b === 0) return "can't divide by 0!";
+      if (b === 0) console.log("can't divide by 0!");
       return a / b;
     case "subtract":
       return a - b;
     default:
-      throw new Error("Operation is not multiply, add, divide, or subtract!");
+      console.log("Operation is not multiply, add, divide, or subtract!");
   }
 };
 
-console.log(calculator(1, 2, "add"));
+const parseArguments = (args: Array<string>) => {
+  if (args.length < 5) throw new Error("Not enough arguments");
+  if (args.length > 5) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    if (typeof args[4] === "string") {
+      const a: number = Number(args[2]);
+      const b: number = Number(args[3]);
+      const op: Operations = args[4] as Operations;
+      console.log(a, b, op);
+
+      return { a, b, op };
+    } else {
+      throw new Error("Provided operation were not valid!");
+    }
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+const cleanArguments = parseArguments(process.argv);
+
+console.log(calculator(cleanArguments.a, cleanArguments.b, cleanArguments.op));
