d1c07b020aff13ccc61e0df733576075562281b9
Martín <martingaliottimartinez@gmail.com>
2024-02-04 17:24:21 -0300

    Interfaces

--- objetsAndArrays.ts
+++ objetsAndArrays.ts
@@ -50,3 +50,48 @@ const board: GameBoard = [
   ["", "", ""],
   ["", "", ""],
 ];
+
+// Interfaces
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+interface ProductOpps {
+  add: (product: Product) => void;
+  remove: (id: string) => void;
+  clear: () => void;
+}
+
+interface ProductCart {
+  products: Product[];
+  totalPrice: number;
+}
+
+const productCart: ProductCart = {
+  totalPrice: 1000,
+  products: [
+    {
+      id: crypto.randomUUID(),
+      name: "random",
+      price: 100,
+      quantity: 10,
+    },
+  ],
+};
+
+// Extender interface
+interface SpecificProduct extends Product {
+  specificProperty: string;
+}
+
+const specificProduct: SpecificProduct = {
+  id: crypto.randomUUID(),
+  name: "random",
+  price: 100,
+  quantity: 10,
+  specificProperty: "random",
+};
